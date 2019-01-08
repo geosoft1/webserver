c143cd28bd73c5d01b8c230622b59605d7a62f38
geosoft1 <geosoft1@gmail.com>
2019-01-08 15:16:53 +0200

    -

--- main.go
+++ main.go
@@ -28,7 +28,28 @@ func main() {
 	router.PathPrefix("/www/").Handler(http.StripPrefix("/www/", http.FileServer(http.Dir(filepath.Join(path, "www")))))
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		templ.ExecuteTemplate(w, "index", nil)
-		log.Println(r.RemoteAddr, r.RequestURI)
+	})
+
+	// setting a middleware (https://github.com/gorilla/mux#middleware)
+	router.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Println(r.RemoteAddr, r.RequestURI)
+			next.ServeHTTP(w, r)
+		})
+	})
+
+	// https://www.google.com/webmasters/tools/home?hl=en
+
+	// google site verification file
+	// https://www.google.com/webmasters/verification/home?hl=en
+	router.HandleFunc("/{google-site-verification}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		http.ServeFile(w, r, filepath.Join(path, vars["google-site-verification"]))
+	})
+	// google robots tester
+	// https://www.google.com/webmasters/tools/robots-testing-tool
+	router.HandleFunc("/robots.txt", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, filepath.Join(path, "robots.txt"))
 	})
 
 	http.ListenAndServe(":"+*port, router)
