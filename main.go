21554988117bfb96f8219e587d29b7296845bbc8
geosoft1 <geosoft1@gmail.com>
2019-01-09 00:29:18 +0200

    -

--- main.go
+++ main.go
@@ -30,27 +30,5 @@ func main() {
 		templ.ExecuteTemplate(w, "index", nil)
 	})
 
-	// setting a middleware (https://github.com/gorilla/mux#middleware)
-	router.Use(func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			log.Println(r.RemoteAddr, r.RequestURI)
-			next.ServeHTTP(w, r)
-		})
-	})
-
-	// https://www.google.com/webmasters/tools/home?hl=en
-
-	// google site verification file
-	// https://www.google.com/webmasters/verification/home?hl=en
-	router.HandleFunc("/{google-site-verification}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		http.ServeFile(w, r, filepath.Join(path, vars["google-site-verification"]))
-	})
-	// google robots tester
-	// https://www.google.com/webmasters/tools/robots-testing-tool
-	router.HandleFunc("/robots.txt", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, filepath.Join(path, "robots.txt"))
-	})
-
 	http.ListenAndServe(":"+*port, router)
 }
