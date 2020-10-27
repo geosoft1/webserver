87f019dc9067b1ef3db1612e22e1dbf7da1fe7dd
geosoft1 <geosoft1@gmail.com>
2020-10-27 10:46:51 +0200

    -

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 
 var httpPort = flag.String("http", ":8080", "http port")
 var httpsPort = flag.String("https", ":8090", "https port")
+var httpRedirect = flag.Bool("http-redirect", false, "if true redirect http to https")
 var crtFile = flag.String("crt", "server.crt", "certificate public key")
 var keyFile = flag.String("key", "server.key", "certificate private key")
 var files = flag.String("files", "static", "html files location")
@@ -30,12 +31,16 @@ func main() {
 		// allow using self signed certificates
 		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 		if err := http.ListenAndServeTLS(*httpsPort, *crtFile, *keyFile, http.FileServer(http.Dir(*files))); err != nil {
-			log.Fatalln(err)
+			log.Println(err)
 		}
 	}()
-	if err := http.ListenAndServe(*httpPort, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "https://"+strings.Split(r.Host, ":")[0]+*httpsPort+r.RequestURI, http.StatusMovedPermanently)
-	})); err != nil {
+	h := http.FileServer(http.Dir(*files))
+	if *httpRedirect {
+		h = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, "https://"+strings.Split(r.Host, ":")[0]+*httpsPort+r.RequestURI, http.StatusMovedPermanently)
+		})
+	}
+	if err := http.ListenAndServe(*httpPort, h); err != nil {
 		log.Fatalln(err)
 	}
 }
